4affab6d87a745f9760ae6dd9aa560aa7baa6810
JoeS <sabe12070@gmail.com>
2021-02-20 07:12:11 -0800

    linting

---
+++ src/App/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+
+import { App } from "./App";
+
+test("renders text", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Header Text/i);
+  expect(linkElement).toBeInTheDocument();
+});
