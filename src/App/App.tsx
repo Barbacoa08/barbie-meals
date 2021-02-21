8fb175a8f1b505cdc3e9181fe7905da4724931ed
JoeS <sabe12070@gmail.com>
2021-02-21 14:27:03 -0800

    use reactn with localStorage

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,39 @@
 import { Center, Container, Flex, Heading } from "@chakra-ui/layout";
+import { useEffect, useGlobal } from "reactn";
+import { State } from "reactn/default";
 
 import { LeftNav, RouterContainer } from "navigation";
 
 import "./App.css";
 
+const localStorageKey = "barbie-meals";
+const defaultLocalStorageValue: State = {
+  showFancy: true,
+  showImages: true,
+};
+
 export const App = () => {
+  const [globals, setGlobals] = useGlobal();
+
+  // on init, pull localStorage once-and-only-once
+  useEffect(() => {
+    const storedValue = window.localStorage.getItem(localStorageKey);
+    const parsedObject = storedValue
+      ? JSON.parse(storedValue)
+      : defaultLocalStorageValue;
+
+    setGlobals(parsedObject);
+  }, []);
+
+  // update localStorage on every reactn update
+  useEffect(() => {
+    const storageObject = Object.keys(globals).length
+      ? { ...globals }
+      : defaultLocalStorageValue;
+
+    window.localStorage.setItem(localStorageKey, JSON.stringify(storageObject));
+  }, [JSON.stringify({ ...globals })]);
+
   return (
     <main className="app" data-testid="App-root">
       <Flex>
--- src/utils/useLocalStorage/useLocalStorage.test.js
+++
@@ -1,4 +0,0 @@
-// NOTE: Invalid hook call. Hooks can only be called inside of the body of a function component.
-it("bogus test", () => {
-  expect(1).toBe(1);
-});
