9bcf895eed0895a250fb4f85ebcf83699ed66e2d
JoeS <sabe12070@gmail.com>
2021-03-07 10:48:32 -0800

    working properly for handhelds, but not monitor
    I want the navigation to ALWAYS be shown for monitors, no hiding

--- src/navigation/LeftNav/LeftNav.tsx
+++ src/navigation/LeftNav/LeftNav.tsx
@@ -1,12 +1,25 @@
 import { useColorMode } from "@chakra-ui/color-mode";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import { Box, Heading, Link, List, ListItem, Stack } from "@chakra-ui/layout";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerOverlay,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { Link as ReachLink } from "@reach/router";
+import { useRef } from "react";
+import { useGlobal } from "reactn";
 
 import { ToggleButton } from "components";
 import { routes } from "navigation";
-import { useGlobal } from "reactn";
 
 export const LeftNav = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const btnRef = useRef<any>();
   const { colorMode, toggleColorMode } = useColorMode();
 
   const [showImages, setShowImages] = useGlobal("showImages");
@@ -17,33 +30,40 @@ export const LeftNav = () => {
   );
 
   return (
-    <Box
-      as="nav"
-      data-testid="LeftNav-root"
-      maxHeight="100vh"
-      maxWidth="sm"
-      overflow="auto"
-    >
-      <Heading size="sm" mt="10" mb="2">
-        Recipes
-      </Heading>
-      <List textAlign="right">
-        <ListItem>
-          <Link as={ReachLink} to={routes.homepage}>
-            Home
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link as={ReachLink} to={routes.sloppyJoes}>
-            Sloppy Joes
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link as={ReachLink} to={routes.pbj}>
-            Toasted PBJ
-          </Link>
-        </ListItem>
-        {/*
+    <Box as="nav" data-testid="LeftNav-root" maxHeight="100vh" ref={btnRef}>
+      <IconButton
+        aria-label="Nav Menu Button"
+        finalFocusRef={btnRef}
+        icon={<HamburgerIcon />}
+        margin="1"
+        onClick={onOpen}
+      />
+      <Drawer placement="left" isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+
+            <DrawerBody>
+              <Heading size="sm" mt="10" mb="2">
+                Recipes
+              </Heading>
+              <List textAlign="right">
+                <ListItem>
+                  <Link as={ReachLink} to={routes.homepage}>
+                    Home
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link as={ReachLink} to={routes.sloppyJoes}>
+                    Sloppy Joes
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link as={ReachLink} to={routes.pbj}>
+                    Toasted PBJ
+                  </Link>
+                </ListItem>
+                {/*
         <ListItem>
           <Link as={ReachLink} to={routes.todo}>
             Pasta
@@ -64,56 +84,60 @@ export const LeftNav = () => {
             Tacos
           </Link>
         </ListItem> */}
-      </List>
+              </List>
 
-      <Heading size="sm" mt="10">
-        View Options
-      </Heading>
-      <Stack my="2">
-        <ToggleButton
-          isChecked={showImages}
-          label="Images"
-          onChange={(checked) => {
-            setShowOnlyTheBasics(false);
-            setShowImages(checked);
-          }}
-        />
-        <ToggleButton
-          isChecked={showFancy}
-          label="Fancy"
-          onChange={(checked) => {
-            setShowOnlyTheBasics(false);
-            setShowFancy(checked);
-          }}
-        />
-        <ToggleButton
-          isChecked={showOpinions}
-          label="Opinionated"
-          onChange={(checked) => {
-            setShowOnlyTheBasics(false);
-            setShowOpinions(checked);
-          }}
-        />
+              <Heading size="sm" mt="10">
+                View Options
+              </Heading>
+              <Stack my="2">
+                <ToggleButton
+                  isChecked={showImages}
+                  label="Images"
+                  onChange={(checked) => {
+                    setShowOnlyTheBasics(false);
+                    setShowImages(checked);
+                  }}
+                />
+                <ToggleButton
+                  isChecked={showFancy}
+                  label="Fancy"
+                  onChange={(checked) => {
+                    setShowOnlyTheBasics(false);
+                    setShowFancy(checked);
+                  }}
+                />
+                <ToggleButton
+                  isChecked={showOpinions}
+                  label="Opinionated"
+                  onChange={(checked) => {
+                    setShowOnlyTheBasics(false);
+                    setShowOpinions(checked);
+                  }}
+                />
 
-        <ToggleButton
-          isChecked={showOnlyTheBasics}
-          label="Only the Basics"
-          onChange={(checked) => {
-            if (checked) {
-              setShowFancy(false);
-              setShowImages(false);
-              setShowOpinions(false);
-            }
+                <ToggleButton
+                  isChecked={showOnlyTheBasics}
+                  label="Only the Basics"
+                  onChange={(checked) => {
+                    if (checked) {
+                      setShowFancy(false);
+                      setShowImages(false);
+                      setShowOpinions(false);
+                    }
 
-            setShowOnlyTheBasics(checked);
-          }}
-        />
-        <ToggleButton
-          isChecked={colorMode === "dark"}
-          label={"Color Mode"}
-          onChange={toggleColorMode}
-        />
-      </Stack>
+                    setShowOnlyTheBasics(checked);
+                  }}
+                />
+                <ToggleButton
+                  isChecked={colorMode === "dark"}
+                  label={"Color Mode"}
+                  onChange={toggleColorMode}
+                />
+              </Stack>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
     </Box>
   );
 };
