55df703fbf48deb9cd2c77b6ab1f8d49fa50a22b
JoeS <sabe12070@gmail.com>
2021-04-30 17:28:19 -0700

    add chicken flautas to costco list
    them things be delicious! (probably super bad for you though)

--- src/components/FoodStores/CostCo/CostCo.tsx
+++ src/components/FoodStores/CostCo/CostCo.tsx
@@ -4,6 +4,7 @@ import { useGlobal } from "reactn";
 
 import { Ingredient } from "components";
 import cheesecurds from "images/costco/costco_cheese-curds.webp";
+import flautas from "images/costco/costco-flautas.webp";
 
 export const CostCo = (_: RouteComponentProps) => {
   const [showImages] = useGlobal("showImages");
@@ -19,6 +20,13 @@ export const CostCo = (_: RouteComponentProps) => {
           img={cheesecurds}
           ingredient="cheese curds"
           ingredientOpinionated="cheese curds"
+          portion=""
+        />
+
+        <Ingredient
+          img={flautas}
+          ingredient="chicken flautas"
+          ingredientOpinionated="chicken flautas"
           portion=""
         />
       </Flex>
