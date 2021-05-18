05324e8989de2452c19d2e491965fb1a951177e5
Barbacoa08 <sabe12070@gmail.com>
2021-05-18 09:13:18 -0700

    allow user to zoom on image (using modal) (#18)
    * shouldn't have empty labels
    
    * this works, but I'm unsure if a "modal" style thing would be better...
    
    * use modal to display larger image
    
    * add base unit tests
    
    * add proper hover functionality

---
+++ src/components/Ingredient/Ingredient.test.jsx
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+
+import { Ingredient } from ".";
+
+describe("Ingredient Component", () => {
+  it("fully renders without exploding", () => {
+    const { getByTestId } = render(
+      <Ingredient
+        img={null}
+        ingredient="example"
+        ingredientOpinionated=""
+        portion="1/2 cup"
+      />
+    );
+
+    const rootElement = getByTestId("Ingredient-root");
+    expect(rootElement).toBeTruthy();
+  });
+});
--- src/components/Ingredient/Ingredient.tsx
+++ src/components/Ingredient/Ingredient.tsx
@@ -1,5 +1,11 @@
 import { Image } from "@chakra-ui/image";
 import { Badge, Box, Text } from "@chakra-ui/layout";
+import {
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useGlobal } from "reactn";
 
 interface IIngredient {
@@ -19,21 +25,41 @@ export const Ingredient = ({
   const [showOpinions] = useGlobal("showOpinions");
   // TODO: useEffect, if any of these change, fade(transition) change. Probably need a new component.
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Box textAlign="center">
+    <Box textAlign="center" data-testid="Ingredient-root">
       <Badge margin="0px 2px 2px 2px" whiteSpace="break-spaces">
         {showOpinions ? ingredientOpinionated : ingredient}
       </Badge>
       <Text>{portion}</Text>
-      {img && <Image
-        alt={ingredientOpinionated}
-        boxSize="150px"
-        fallbackSrc="https://via.placeholder.com/150"
-        hidden={!showImages}
-        margin="auto"
-        objectFit="contain"
-        src={img}
-      />}
+      {img && (
+        <Image
+          alt={ingredientOpinionated}
+          boxSize="150px"
+          cursor="zoom-in"
+          fallbackSrc="https://via.placeholder.com/150"
+          hidden={!showImages}
+          margin="auto"
+          objectFit="contain"
+          onClick={onOpen}
+          src={img}
+        />
+      )}
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <Image
+            alt={ingredientOpinionated}
+            fallbackSrc="https://via.placeholder.com/150"
+            hidden={!showImages}
+            margin="auto"
+            objectFit="contain"
+            src={img}
+          />
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
