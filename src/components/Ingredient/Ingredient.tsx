95759a88fab0e80f697094063284b8cf08411766
JoeS <sabe12070@gmail.com>
2021-05-18 07:37:09 -0700

    this works, but I'm unsure if a "modal" style thing would be better...

--- src/components/Ingredient/Ingredient.tsx
+++ src/components/Ingredient/Ingredient.tsx
@@ -1,6 +1,8 @@
 import { Image } from "@chakra-ui/image";
 import { Badge, Box, Text } from "@chakra-ui/layout";
-import { useGlobal } from "reactn";
+import { useGlobal, useMemo } from "reactn";
+
+import "./Ingredient.css";
 
 interface IIngredient {
   img?: string;
@@ -19,21 +21,33 @@ export const Ingredient = ({
   const [showOpinions] = useGlobal("showOpinions");
   // TODO: useEffect, if any of these change, fade(transition) change. Probably need a new component.
 
+  const inputId = useMemo(
+    () => `input-click-zoom-${ingredient.replace(/ /g, "_")}`,
+    [ingredient]
+  );
+
   return (
     <Box textAlign="center">
       <Badge margin="0px 2px 2px 2px" whiteSpace="break-spaces">
         {showOpinions ? ingredientOpinionated : ingredient}
       </Badge>
       <Text>{portion}</Text>
-      {img && <Image
-        alt={ingredientOpinionated}
-        boxSize="150px"
-        fallbackSrc="https://via.placeholder.com/150"
-        hidden={!showImages}
-        margin="auto"
-        objectFit="contain"
-        src={img}
-      />}
+      {img && (
+        <Box className="click-zoom">
+          <label id={`click-zoom-${ingredient}`} htmlFor={inputId}>
+            <input type="checkbox" id={inputId} />
+            <Image
+              alt={ingredientOpinionated}
+              boxSize="150px"
+              fallbackSrc="https://via.placeholder.com/150"
+              hidden={!showImages}
+              margin="auto"
+              objectFit="contain"
+              src={img}
+            />
+          </label>
+        </Box>
+      )}
     </Box>
   );
 };
