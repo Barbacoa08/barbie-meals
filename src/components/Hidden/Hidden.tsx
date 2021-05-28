a20cf7ee1de3b691b0cb72a62c283944d879ec6f
JoeS <sabe12070@gmail.com>
2021-05-28 16:28:06 -0700

    WIP: temp, all brokeded

--- src/components/Hidden/Hidden.tsx
+++ src/components/Hidden/Hidden.tsx
@@ -5,29 +5,34 @@ import { useState } from "react";
 
 import ApolloClient, { gql } from "apollo-boost";
 import { ApolloProvider, Query } from "react-apollo";
+import { useQuery } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "/.netlify/functions/graphql",
 });
-const LambdaDemo = () => (
-  <ApolloProvider client={client}>
-    <Query
-      query={gql`
-        {
-          hello
-        }
-      `}
-    >
-      {({ data = { hello: "" } }) => (
-        <div>A greeting from the server: {data.hello}</div>
-      )}
-    </Query>
-  </ApolloProvider>
-);
+
+// export const NUMBER_QUERY = gql`
+//   query NumberQuery($lower: Int, $upper: Int) {
+//     multiply(lower: $lower, upper: $upper)
+//   }
+// `;
+
+const PING_QUERY = gql`
+  {
+    ping
+  }
+`;
+// export const PING_QUERY = gql`
+//   query PingQuery() {
+//     ping
+//   }
+// `;
 
 export const Hidden = (_: RouteComponentProps) => {
   const [lambdaResult, setLambdaResult] = useState("initial state");
   const [gqlResult, setGQLResult] = useState("GraphQL's initial state");
+
+  const { data } = useQuery(PING_QUERY);
 
   return (
     <Container data-testid="Hidden-root">
@@ -67,9 +72,8 @@ export const Hidden = (_: RouteComponentProps) => {
             variant="outline"
             onClick={() => {
               try {
-                // fetch("/.netlify/functions/hello")
-                //   .then((data) => data.json())
-                //   .then((body) => setLambdaResult(body.message));
+                // const { data } = useQuery(PING_QUERY);
+                console.log(data);
               } catch (_e) {
                 setGQLResult("hit, no result");
               }
@@ -82,8 +86,21 @@ export const Hidden = (_: RouteComponentProps) => {
         <Text>Results:</Text>
         <Code>{gqlResult}</Code>
 
-        <Code paddingTop="10">
-          <LambdaDemo />
+        <Text paddingTop="10">LambdaDemo Results:</Text>
+        <Code>
+          <ApolloProvider client={client}>
+            <Query
+              query={gql`
+                {
+                  hello
+                }
+              `}
+            >
+              {({ data = { hello: "" } }) => (
+                <div>A greeting from the server: {data.hello}</div>
+              )}
+            </Query>
+          </ApolloProvider>
         </Code>
       </Box>
     </Container>
--- src/lambda-functions/graphql.js
+++ src/lambda-functions/graphql.js
@@ -4,12 +4,25 @@ const typeDefs = gql`
   type Query {
     hello: String
   }
+  type Query {
+    ping: String
+  }
+  type Query {
+    multiply(lower: Int, upper: Int): Int
+  }
 `;
 
 const resolvers = {
   Query: {
     hello: (root, args, context) => {
       return "Hello, world!";
+    },
+    ping: () => {
+      console.log("ping");
+      return "ping!";
+    },
+    multiply: (lower, upper) => {
+      return lower * upper;
     },
   },
 };
