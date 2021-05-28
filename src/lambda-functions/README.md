c0122a6647d524172c9793d4a9e5db77c4b2f521
JoeS <sabe12070@gmail.com>
2021-05-28 14:21:41 -0700

    working... using recommended babelrc settings

--- src/lambda-functions/README.md
+++ src/lambda-functions/README.md
@@ -2,3 +2,4 @@
 
 - http://localhost:9000/.netlify/functions/hello
 - largly based off of [this repo](https://github.com/netlify/create-react-app-lambda) ([ts version here](https://github.com/sw-yx/create-react-app-lambda-typescript)), which links to [this youtube demo](https://youtu.be/3ldSM98nCHI)
+- notes on [TS w/ AWS Lambda functions](https://levelup.gitconnected.com/how-to-use-typescript-for-aws-lambda-in-3-steps-1996243547eb)
--- src/lambda-functions/hello.ts
+++ src/lambda-functions/hello.ts
@@ -1,21 +1,12 @@
-// import { Context, Callback, APIGatewayEvent } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-// exports.handler = function (
-//   _event: APIGatewayEvent,
-//   _context: Context,
-//   callback: Callback
-// ) {
-//   callback(null, {
-//     statusCode: 200,
-//     body: JSON.stringify({ message: `ping: ${Math.random()}` }),
-//   });
-// };
-
-export async function handler() {
+export const handler = async (
+  _event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: `ping ${Math.floor(Math.random() * 10)}`,
     }),
   };
-}
+};
