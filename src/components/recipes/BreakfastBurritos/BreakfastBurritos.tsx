665d8ad3619d0f20352b852bf46e71249417a849
JoeS <sabe12070@gmail.com>
2021-09-06 08:44:12 -0700

    setup the base of the breakfast burrito recipe

--- src/components/recipes/BreakfastBurritos/BreakfastBurritos.tsx
+++ src/components/recipes/BreakfastBurritos/BreakfastBurritos.tsx
@@ -1,5 +1,40 @@
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Grid,
+  Heading,
+  Link,
+  ListItem,
+  OrderedList,
+  Stack,
+  Text,
+} from "@chakra-ui/layout";
+import { Tag } from "@chakra-ui/tag";
 import { RouteComponentProps } from "@reach/router";
 
+import { Ingredient } from "components";
+import ketchup from "images/condiments/ketchup.webp";
+import buns from "images/misc/buns.webp";
+import meat from "images/misc/impossible-meat.webp";
+import pepper from "images/seasonings/black-pepper.webp";
+import mexispice from "images/seasonings/mexi-spice.webp";
+import salt from "images/seasonings/salt.webp";
+
 export const BreakfastBurritos = (_: RouteComponentProps) => {
-  return <main data-testid="BreakfastBurritos-root">ping</main>;
+  return (
+    <Stack data-testid="BreakfastBurritos-root">
+      <Flex justifyContent="space-evenly">
+        <Heading size="lg">Burgers</Heading>
+
+        <Box display="inline-flex">
+          <Tag>#filling</Tag>
+          <Tag>#fast</Tag>
+          <Tag>#easy</Tag>
+        </Box>
+      </Flex>
+
+      <Grid>TODO: ingredients</Grid>
+    </Stack>
+  );
 };
