e632f17e35d1ebcfcaf29c4ba213c00a2b641804
JoeS <sabe12070@gmail.com>
2022-02-26 14:10:11 -0800

    simplify `Favorites` by moving some logic into a helper file

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,21 +1,14 @@
-import { AddIcon, MinusIcon } from "@chakra-ui/icons";
-import { Flex, Heading, Link, Stack } from "@chakra-ui/layout";
-import {
-  Divider,
-  IconButton,
-  Input,
-  ScaleFade,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { Link as ReachLink, RouteComponentProps } from "@reach/router";
+import { Flex, Heading, Stack } from "@chakra-ui/layout";
+import { Divider, Input } from "@chakra-ui/react";
+import { RouteComponentProps } from "@reach/router";
 import { useCallback, useEffect, useState } from "react";
 import { usePouch } from "use-pouchdb";
 
 import { routes } from "navigation";
-import { hashValue, stringCamelCaseToSentence } from "utils";
 
 import { getUserFavorites } from "../../graphql";
-import { FavoriteOptionProps, PouchFavorites } from "./FavoritesTypes";
+import { PouchFavorites } from "./FavoritesTypes";
+import { calculateFavorites } from "./helpers";
 
 export const Favorites = (_: RouteComponentProps) => {
   const { alcohol, recipes } = routes;
@@ -29,180 +22,13 @@ export const Favorites = (_: RouteComponentProps) => {
 
   const [user, setUser] = useState<string>("");
 
-  // TODO: this method is so... yuck... find a cleaner solution
-  const calculateMeals = useCallback(
-    (storedFavoriteMeals: PouchDB.Core.AllDocsResponse<{}>) => {
-      const storedIds = storedFavoriteMeals.rows.map((row) => row.id);
-
-      const addtMeals: JSX.Element[] = [];
-      const favMeals: JSX.Element[] = [];
-      Object.keys(recipes).forEach((mealId) => {
-        const uri = recipes[mealId];
-        const title = stringCamelCaseToSentence(mealId);
-        const key = `meal-option-${mealId}`;
-
-        if (storedIds.includes(mealId)) {
-          favMeals.push(
-            <FavoriteOption
-              icon="remove"
-              title={title}
-              linkTo={uri}
-              key={key}
-              onClick={(e) => {
-                e.preventDefault();
-
-                dbFavorites.get(mealId).then((doc) => {
-                  dbFavorites
-                    .remove(doc)
-                    .then(() => {
-                      dbFavorites
-                        .allDocs()
-                        .then(calculateMeals)
-                        .catch((err) =>
-                          console.error("PouchDb.allDocs err", err)
-                        );
-                    })
-                    .catch((err) => console.error("meal removal err", err));
-                });
-              }}
-            />
-          );
-        } else {
-          addtMeals.push(
-            <FavoriteOption
-              icon="add"
-              title={title}
-              linkTo={uri}
-              key={key}
-              onClick={(e) => {
-                e.preventDefault();
-
-                const meal: PouchFavorites = {
-                  _id: mealId,
-                  title,
-                  icon: "add",
-                  linkTo: uri,
-                  key,
-                };
-                dbFavorites
-                  .put(meal)
-                  .then(() => {
-                    dbFavorites
-                      .allDocs()
-                      .then(calculateMeals)
-                      .catch((err) =>
-                        console.error("PouchDb.allDocs err", err)
-                      );
-                  })
-                  .catch((err) => console.error("meal PUT err", err));
-              }}
-            />
-          );
-        }
-      });
-
-      const favMealsUpdated = hashValue(favMeals) !== hashValue(meals);
-      const addtMealsUpdated =
-        hashValue(addtMeals) !== hashValue(additionalMeals);
-
-      if (favMealsUpdated || addtMealsUpdated) {
-        setMeals(favMeals);
-        setAdditionalMeals(addtMeals);
-      }
-    },
-    [recipes]
-  );
-
-  const calculateDrinks = useCallback(
-    (storedFavoriteDrinks: PouchDB.Core.AllDocsResponse<{}>) => {
-      const storedIds = storedFavoriteDrinks.rows.map((row) => row.id);
-
-      const addtDrinks: JSX.Element[] = [];
-      const favDrinks: JSX.Element[] = [];
-      Object.keys(alcohol).forEach((drinkId) => {
-        const uri = alcohol[drinkId];
-        const title = stringCamelCaseToSentence(drinkId);
-        const key = `drink-option-${drinkId}`;
-
-        if (storedIds.includes(drinkId)) {
-          favDrinks.push(
-            <FavoriteOption
-              icon="remove"
-              title={title}
-              linkTo={uri}
-              key={key}
-              onClick={(e) => {
-                e.preventDefault();
-
-                dbFavorites.get(drinkId).then((doc) => {
-                  dbFavorites
-                    .remove(doc)
-                    .then(() => {
-                      dbFavorites
-                        .allDocs()
-                        .then(calculateDrinks)
-                        .catch((err) =>
-                          console.error("PouchDb.allDocs err", err)
-                        );
-                    })
-                    .catch((err) => console.error("drink removal err", err));
-                });
-              }}
-            />
-          );
-        } else {
-          addtDrinks.push(
-            <FavoriteOption
-              icon="add"
-              title={title}
-              linkTo={uri}
-              key={key}
-              onClick={(e) => {
-                e.preventDefault();
-
-                const drink: PouchFavorites = {
-                  _id: drinkId,
-                  title,
-                  icon: "add",
-                  linkTo: uri,
-                  key,
-                };
-                dbFavorites
-                  .put(drink)
-                  .then(() => {
-                    dbFavorites
-                      .allDocs()
-                      .then(calculateDrinks)
-                      .catch((err) =>
-                        console.error("PouchDb.allDocs err", err)
-                      );
-                  })
-                  .catch((err) => console.error("drink PUT err", err));
-              }}
-            />
-          );
-        }
-      });
-
-      const favDrinksUpdated = hashValue(favDrinks) !== hashValue(drinks);
-      const addtDrinksUpdated =
-        hashValue(addtDrinks) !== hashValue(additionalDrinks);
-
-      if (favDrinksUpdated || addtDrinksUpdated) {
-        setDrinks(favDrinks);
-        setAdditionalDrinks(addtDrinks);
-      }
-    },
-    [alcohol]
-  );
-
   const pullData = useCallback(async (user: string = "") => {
     // TODO: implement usage
     const dbResult = await getUserFavorites(user);
     console.log("dbResult", dbResult);
-    // TODO: notes: pull data, if "favorites" doesn't exist, kill it
+    // TODO: notes: pull data, if "favorite" doesn't exist, kill it
 
-    // TODO: notes: implement add/remove favorites
+    // TODO: notes: implement add/remove favorite
 
     // still need to setup `sync`: https://pouchdb.com/api.html#sync
     // https://hasura.io/blog/couchdb-style-conflict-resolution-rxdb-hasura/
@@ -210,8 +36,20 @@ export const Favorites = (_: RouteComponentProps) => {
     dbFavorites
       .allDocs()
       .then((result) => {
-        calculateDrinks(result);
-        calculateMeals(result);
+        calculateFavorites(
+          result,
+          alcohol,
+          dbFavorites,
+          setDrinks,
+          setAdditionalDrinks
+        );
+        calculateFavorites(
+          result,
+          recipes,
+          dbFavorites,
+          setMeals,
+          setAdditionalMeals
+        );
       })
       .catch((err) => console.error("PouchDb.allDocs err", err));
   }, []);
@@ -280,37 +118,5 @@ export const Favorites = (_: RouteComponentProps) => {
         </Stack>
       </Flex>
     </Stack>
-  );
-};
-
-const FavoriteOption = ({
-  icon,
-  title,
-  linkTo,
-  onClick,
-}: FavoriteOptionProps) => {
-  const { isOpen, onToggle } = useDisclosure({ isOpen: true });
-
-  const buttonIcon = icon === "add" ? <AddIcon /> : <MinusIcon />;
-
-  return (
-    <ScaleFade initialScale={0.1} in={isOpen}>
-      <Flex borderWidth="2px" justifyContent="space-between" p={3} shadow="md">
-        <Stack justify="center" paddingRight={5}>
-          <Link as={ReachLink} to={linkTo}>
-            {title}
-          </Link>
-        </Stack>
-
-        <IconButton
-          aria-label={icon}
-          icon={buttonIcon}
-          onClick={(e) => {
-            onToggle();
-            onClick(e);
-          }}
-        />
-      </Flex>
-    </ScaleFade>
   );
 };
---
+++ src/components/Favorites/helpers.tsx
@@ -0,0 +1,134 @@
+import { AddIcon, MinusIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  IconButton,
+  Link,
+  ScaleFade,
+  Stack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { Link as ReachLink } from "@reach/router";
+import { Dispatch, SetStateAction } from "react";
+
+import { stringCamelCaseToSentence } from "utils";
+
+import { FavoriteOptionProps, PouchFavorites } from "./FavoritesTypes";
+
+export const calculateFavorites = (
+  storedFavorites: PouchDB.Core.AllDocsResponse<{}>,
+  availableItems: { [key: string]: string },
+  dbFavorites: PouchDB.Database<PouchFavorites>,
+  setSelectedFavorites: Dispatch<SetStateAction<JSX.Element[]>>,
+  setAdditionalFavorites: Dispatch<SetStateAction<JSX.Element[]>>
+) => {
+  const handleAddRemoveClick = (
+    storedFavorites: PouchDB.Core.AllDocsResponse<{}>
+  ) =>
+    calculateFavorites(
+      storedFavorites,
+      availableItems,
+      dbFavorites,
+      setSelectedFavorites,
+      setAdditionalFavorites
+    );
+
+  const storedIds = storedFavorites.rows.map((row) => row.id);
+
+  const selectedFavs: JSX.Element[] = [];
+  const addtFavs: JSX.Element[] = [];
+  Object.keys(availableItems).forEach((favoriteId) => {
+    const uri = availableItems[favoriteId];
+    const title = stringCamelCaseToSentence(favoriteId);
+    const key = `favorite-option-${favoriteId}`;
+
+    if (storedIds.includes(favoriteId)) {
+      selectedFavs.push(
+        <FavoriteOption
+          icon="remove"
+          title={title}
+          linkTo={uri}
+          key={key}
+          onClick={(e) => {
+            e.preventDefault();
+
+            dbFavorites.get(favoriteId).then((doc) => {
+              dbFavorites
+                .remove(doc)
+                .then(() => {
+                  dbFavorites
+                    .allDocs()
+                    .then(handleAddRemoveClick)
+                    .catch((err) => console.error("PouchDb.allDocs err", err));
+                })
+                .catch((err) => console.error("favorite removal err", err));
+            });
+          }}
+        />
+      );
+    } else {
+      addtFavs.push(
+        <FavoriteOption
+          icon="add"
+          title={title}
+          linkTo={uri}
+          key={key}
+          onClick={(e) => {
+            e.preventDefault();
+
+            const favToAdd: PouchFavorites = {
+              _id: favoriteId,
+              title,
+              icon: "add",
+              linkTo: uri,
+              key,
+            };
+            dbFavorites
+              .put(favToAdd)
+              .then(() => {
+                dbFavorites
+                  .allDocs()
+                  .then(handleAddRemoveClick)
+                  .catch((err) => console.error("PouchDb.allDocs err", err));
+              })
+              .catch((err) => console.error("favorite PUT err", err));
+          }}
+        />
+      );
+    }
+  });
+
+  setSelectedFavorites(selectedFavs);
+  setAdditionalFavorites(addtFavs);
+};
+
+const FavoriteOption = ({
+  icon,
+  title,
+  linkTo,
+  onClick,
+}: FavoriteOptionProps) => {
+  const { isOpen, onToggle } = useDisclosure({ isOpen: true });
+
+  const buttonIcon = icon === "add" ? <AddIcon /> : <MinusIcon />;
+
+  return (
+    <ScaleFade initialScale={0.1} in={isOpen}>
+      <Flex borderWidth="2px" justifyContent="space-between" p={3} shadow="md">
+        <Stack justify="center" paddingRight={5}>
+          <Link as={ReachLink} to={linkTo}>
+            {title}
+          </Link>
+        </Stack>
+
+        <IconButton
+          aria-label={icon}
+          icon={buttonIcon}
+          onClick={(e) => {
+            onToggle();
+            onClick(e);
+          }}
+        />
+      </Flex>
+    </ScaleFade>
+  );
+};
